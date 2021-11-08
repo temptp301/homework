b1209f3e3b9c8d0d15befdadd61307e0a6d9cc20
tem_ptp <73431449+temptp301@users.noreply.github.com>
2021-11-08 13:51:14 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const { GraphQLServer, PubSub } = require('graphql-yoga');
+
+let defaultName = "tem";
+
+const pubsub = new PubSub();
+
+const typeDefs = `
+	type Query {
+		hello(name: String): String!
+		sayhi: String!
+	}
+
+	type Mutation {
+		changeDefaultName(name: String!): String!
+	}
+
+	type Subscription {
+		updateName: String!
+	}
+`;
+
+const resolvers = {
+	Query: {
+		hello: (root, { name }, ctx, info) => {
+			if (!name)
+				name = defaultName;
+			return `Hello World from ${name}!`;
+		},
+		sayhi: () => "Hi API from graphQL"
+	},
+	Mutation: {
+		changeDefaultName: (root, { name }, ctx, info) => {
+			defaultName = name;
+			pubsub.publish('update_name', {
+				updateName: `Notify Update Default Name to ${name}`
+			})
+			return `Ok change the default name to ${defaultName}`;
+		}
+	},
+	Subscription: {
+		updateName: {
+			subscribe(root, args, ctx, info) {
+				return pubsub.asyncIterator('update_name');
+			}
+		}
+	}
+};
+
+const server = new GraphQLServer({
+	typeDefs,
+	resolvers
+});
+
+const options = {
+	port: 4000,
+	endpoint: '/graphql'
+};
+
+server.start(options, (args) => { 
+	const { port } = args;
+  console.log(`Server start on port: ${port}`)
+});
